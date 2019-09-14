21a66964d661a9efca208c2cea1417e95ac1f09d
york yao <yaoao12306@outlook.com>
2019-09-14 18:41:07 +0800

    feat: add basic features

--- clean-release.config.js
+++ clean-release.config.js
@@ -11,8 +11,13 @@ module.exports = {
   askVersion: true,
   releaseRepository: 'https://github.com/plantain-00/storage-service-release.git',
   postScript: [
-    'cd "[dir]" && rm -rf .git',
-    'cp Dockerfile "[dir]"',
-    'cd "[dir]" && docker build -t plantain-00/storage-service . && docker push plantain-00/storage-service'
+    // 'cd "[dir]" && rm -rf .git',
+    // 'cp Dockerfile "[dir]"',
+    // 'cd "[dir]" && docker build -t plantain-00/storage-service . && docker push plantain-00/storage-service',
+    'git add package.json',
+    'git commit -m "[version]"',
+    'git tag v[version]',
+    'git push',
+    'git push origin v[version]',
   ]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,63 @@
-console.log('app started!')
+import express from 'express'
+import minimist from 'minimist'
+import bodyParser from 'body-parser'
+import fs from 'fs'
+import path from 'path'
+
+const app = express()
+const argv = minimist(process.argv.slice(2)) as {
+  p?: number
+  h?: string
+}
+const port = argv.p || 9245
+const host = argv.h || 'localhost'
+
+const filesPath = path.resolve(__dirname, '../files')
+try {
+  fs.mkdirSync(filesPath)
+} catch {
+  // do nothing
+}
+
+app.use(bodyParser.json())
+
+function setCors(res: express.Response) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+}
+
+app.get('/:key', (req, res) => {
+  const filePath = path.resolve(filesPath, req.params.key)
+  fs.stat(filePath, (error) => {
+    if (error) {
+      res.status(404)
+      res.end(error.message)
+    } else {
+      res.setHeader('Content-Type', 'application/json')
+      setCors(res)
+      res.send(fs.readFileSync(filePath))
+    }
+  })
+})
+
+app.post('/:key', (req, res) => {
+  const filePath = path.resolve(filesPath, req.params.key)
+  fs.writeFile(filePath, JSON.stringify((req as { body: unknown }).body), (error) => {
+    if (error) {
+      res.status(400)
+      res.end(error.message)
+    } else {
+      res.setHeader('Content-Type', 'application/json')
+      setCors(res)
+      res.send(fs.readFileSync(filePath))
+    }
+  })
+})
+
+app.listen(port, host, () => {
+  console.log(`storage service is listening: ${host}:${port}`)
+})
 
 process.on('SIGINT', () => {
   process.exit()
